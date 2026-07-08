d3b85eec91e19bad24f92796d8725ad2667512dc
Japhet Sunday <japhet996sunday@gmail.com>
2026-07-08 20:28:05 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+import { initTheme } from "./utils/theme.js";
+import { initRippleEffect } from "./utils/ripple.js";
+import { initNavigation } from "./components/navigation.js";
+
+function bootstrap() {
+  initTheme();
+  initNavigation();
+  initRippleEffect();
+}
+
+document.addEventListener("DOMContentLoaded", bootstrap);
 
