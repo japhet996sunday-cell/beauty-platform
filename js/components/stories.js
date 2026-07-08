a89a034784c9fa2f7a18737611b9e5d5faec9afe
Japhet Sunday <japhet996sunday@gmail.com>
2026-07-08 20:54:25 +0100

    Add renderStoriesRail function to render stories

--- js/components/stories.js
+++ js/components/stories.js
@@ -1,1 +1,31 @@
+export function renderStoriesRail(stories) {
+  return stories
+    .map((story) => {
+      if (story.isOwn) {
+        return `
+          <li class="story-item">
+            <button type="button" class="story-avatar-btn" data-story-id="${story.id}" aria-label="Add to your story">
+              <span class="story-ring story-ring--own">
+                <img class="avatar avatar-md avatar-ring-inner" src="${story.user.avatar}" alt="">
+                <span class="story-add-icon" aria-hidden="true">+</span>
+              </span>
+              <span class="story-label">Your story</span>
+            </button>
+          </li>
+        `;
+      }
+
+      return `
+        <li class="story-item">
+          <button type="button" class="story-avatar-btn" data-story-id="${story.id}" aria-label="View ${story.user.name}'s story">
+            <span class="story-ring${story.viewed ? " is-viewed" : ""}">
+              <img class="avatar avatar-md avatar-ring-inner" src="${story.user.avatar}" alt="">
+            </span>
+            <span class="story-label">${story.user.handle}</span>
+          </button>
+        </li>
+      `;
+    })
+    .join("");
+}
 
