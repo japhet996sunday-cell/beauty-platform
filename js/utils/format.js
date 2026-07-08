2c58fc7f24ea58de5d158a897eb08c8634279934
Japhet Sunday <japhet996sunday@gmail.com>
2026-07-08 21:03:05 +0100

    Add formatCount function for number formatting

--- js/utils/format.js
+++ js/utils/format.js
@@ -1,1 +1,6 @@
+export function formatCount(value) {
+  if (value >= 1000000) return `${(value / 1000000).toFixed(1).replace(/\.0$/, "")}M`;
+  if (value >= 1000) return `${(value / 1000).toFixed(1).replace(/\.0$/, "")}K`;
+  return String(value);
+}
 
