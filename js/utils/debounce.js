6a582df4cec1c0b2d52d4f8c3ccccfdfd650233b
Japhet Sunday <japhet996sunday@gmail.com>
2026-07-08 21:02:00 +0100

    Add debounce function for delayed execution

--- js/utils/debounce.js
+++ js/utils/debounce.js
@@ -1,1 +1,8 @@
+export function debounce(fn, delay = 300) {
+  let timeoutId;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn(...args), delay);
+  };
+}
 
